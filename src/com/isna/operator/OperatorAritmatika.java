ea9da0d9757a11b575fd3432b8677404b2ebeb1e
mokhamadisna <mokhammadisna@gmail.com>
2020-11-15 16:33:40 +0700

    p

---
+++ src/com/isna/operator/OperatorAritmatika.java
@@ -0,0 +1,28 @@
+package com.isna.operator;
+
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        System.out.println("Operasi Penjumlahan");
+        int hasilPenjumlahan = 9 + 10;
+        System.out.println("Hasil 9 + 10 = " + hasilPenjumlahan);
+        System.out.println();
+        System.out.println("Operasi Pengurangan");
+        int hasilPengurangan = 9 - 18;
+        System.out.println("Hasil 9 - 18 = " + hasilPengurangan);
+        System.out.println();
+        System.out.println("Operasi Pengalian");
+        int hasilPengalian = 56 * 55;
+        System.out.println("Hasil 56 * 55 = " + hasilPengalian);
+        System.out.println();
+        System.out.println("Operasi Pembagian");
+        int hasilPembagian = 12 / 2;
+        System.out.println("Hasil 12 / 2 = " + hasilPembagian);
+        System.out.println();
+        System.out.println("Operasi Habis bagi");
+        int hasilSisa = 18 % 3;
+        System.out.println("Hasil 18 % 3 = " + hasilSisa);
+        int hasilSisaLain = 9 % 4;
+        System.out.println("Hasil 9 % 4 = " + hasilSisaLain);
+        System.out.println();
+    }
+}
